f45ddc5c81371938e811593343e54cc235c33651
Davvii1 <david2002rg@hotmail.com>
2023-01-27 15:03:11 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const start = new Date("Jan 23, 2023 00:00:00").getTime(); // Jan 1, 2015
+  const end = new Date("Jun 23, 2023 00:00:00").getTime(); // June 24, 2015
+  const today = new Date().getTime();
+  const p = Math.round(((today - start) / (end - start)) * 100);
+
+  return (
+    <div className="outside-container">
+      <div className='inside-container' style={{width: `${p}%`}}>
+        <img src="/public/images/Icon.png" alt="Gengar Icon" />
+      </div>
+    </div>
+  )
+}
+
+export default App
